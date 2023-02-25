e507aa97d57d719ed0d80c7138d51055d338eaf5
Md. Rasel Hossain <raselhossainb@gmail.com>
2023-02-25 22:21:02 +0600

    laravel version update v9.52.4 to laravel v10.1.5 end

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'full_name', 'user_name', 'phone', 'email', 'last_login', 'branch_id', 'permissions', 'm_permission', 'password', 'email_verified_at', 'remember_token'
     ];
 
     /**
