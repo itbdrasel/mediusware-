86d3ff02e41c040abf0c807a67eeb1f86087857f
Md. Rasel Hossain <raselhossainb@gmail.com>
2023-02-27 00:40:57 +0600

    update

---
+++ Modules/Scms/Entities/RulesMarksManage.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Modules\Scms\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+class RulesMarksManage extends Model
+{
+
+    protected $table = 'scms_rules_marks_manage';
+
+    protected $fillable = [
+        'exam_id', 'class_id', 'start_year', 'end_year', 'calculation_subject', 'branch_id', 'vtype'
+    ];
+
+    public static $sortable = ['exam_id', 'class_id'];
+
+    public static $filters = [''];
+
+    public static $required = ['class_id'=>'Class','exam_id'=>'Exam', 'calculation_subject'=>'Total Grade Calculation By Subject'];
+
+    public static $insertData = ['class_group_id','exam_id'];
+
+}
